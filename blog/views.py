763fb9c2a70759c34a4b375f46073cb4912be747
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-10-07 11:11:01 +0530

    mysite_7-10-25

--- blog/views.py
+++ blog/views.py
@@ -1,104 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for the waht happened whilst I was enjoying the view!",
-        "content": """ 
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
 
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-        """             
-                         
-
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming is fun!",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for the waht happened whilst I was enjoying the view!",
-        "content": """ 
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-        """             
-                         
-
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "nature at its best",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for the waht happened whilst I was enjoying the view!",
-        "content": """ 
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem dignissimos, 
-           ad quidem labore molestiae nihil esse excepturi error hic ipsum repellat cupiditate! Deserunt
-           suscipit ab perferendis nihil officia culpa.
-
-        """             
-                         
-
-    }
-]
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 
-
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_Posts = Post.objects.all().order_by("-date")[:3]
     return render( request,"blog/index.html", {
-     "posts": latest_posts
+     "posts": latest_Posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts      
     })
 
 def post_detail(request, slug):
-    identified_post = next(posts for posts in all_posts if posts["slug"] == slug)
+    identified_post = get_object_or_404 (Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post 
+        "post": identified_post, 
+        "post_tags": identified_post.tags.all()
     })
 
 
